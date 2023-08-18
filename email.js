3ed61098e484bef1a263545ec19ea51dfb11e731
RohitKumarSahu <thedevrohit@gmail.com>
2023-08-17 20:09:35 -0700

    My Portfolio First Time Commit

---
+++ email.js
@@ -0,0 +1,43 @@
+function sendMail(){
+    let toast = document.getElementById("toast")
+    
+    if(document.getElementById("email").value == ""){
+        toast.innerHTML="Please enter valid detail !! ";
+        toast.style.color="red";
+        setTimeout(()=>{
+            toast.innerHTML=""; 
+          },2000)
+
+          return ;         
+    }
+    
+    var params= {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    }
+   const serviceId = "service_2l84i7d"
+   const templateId = "template_8ol9nog";
+   
+   emailjs.send(serviceId , templateId , params)
+    .then(
+    (res) => {
+      document.getElementById("name").value="",
+      document.getElementById("email").value="",
+      document.getElementById("message").value=""
+      console.log(res)
+
+      let check="<i class='fa-solid fa-check'></i>";
+      toast.innerHTML="Successfully Send "+check;
+      toast.style.color="green";
+
+      setTimeout(()=>{
+        toast.innerHTML=""; 
+      },3000)
+
+    //   alert("Your message is send succesfully");
+   
+    }).catch((err) => {
+      console.log(err);
+   });   
+}
---
+++ script.js
@@ -0,0 +1,237 @@
+let tl = gsap.timeline();
+
+
+tl.from(".logo" ,{
+    y:-100,
+    duration:2
+})
+
+let tl2 = gsap.timeline(); 
+
+tl2.from("span" ,{
+    y:-600,
+    duration:1,
+    scrollTrigger:{
+      trigger:"span",
+      scroller:"body",
+      scrub:2, 
+      stagger:1,  
+    }
+})
+
+tl2.from(".quality-box" ,{
+    y:-700,
+    duration:3
+})
+gsap.from(".social-box .fb",{
+    y:-500,
+    duration:4,
+    
+ })
+
+gsap.from("#name-box h1",{
+    x:-400,
+})
+
+
+ gsap.from(".main-btn" ,{
+    y:-300,
+    duration:5
+})
+
+let tla = gsap.timeline()
+
+tla.from("#nav-item ",{
+    y:-100,
+    duration:3,
+    stagger:1,
+})
+
+//  About Section 
+
+gsap.from(".about-img-box img",{
+    scale:0.5,
+    duration:2,
+    scrollTrigger:{
+      scroller:"body",
+      trigger:".about-img-box img",   
+      stagger:2,
+      yoyo:true,
+      scrub:2,
+    //   markers:true,
+      start:"top 100%",
+      end:"top 50%"
+    }
+})
+
+
+let aboutText = gsap.timeline();
+
+aboutText.from(".about-right-box h1",{
+     y:-60,
+     duration:2,
+     scrollTrigger : {
+     scroller:"body",
+    //  markers:true,
+     start:"bottom 100%",
+     end:"bottom 20%",
+     trigger:".about-right-box h1",
+     scrub:3,
+    }
+})
+
+aboutText.from(".about-right-box .details .row",{
+    y:-100,
+    duration:1,
+    scrollTrigger : {
+        scroller:"body",
+        trigger:".about-right-box .details .row",
+        scrub:1,
+        // markers:true,
+        start:"bottom 100%",
+        end:"bottom 0"
+    }
+})
+
+//  Detailse Page   
+
+gsap.from(".service-main h1",{
+    scale:0.5,
+    duration:2,
+    scrollTrigger:{
+        trigger:".service-main h1 ",
+        scroller:"body",
+        scrub:2,
+    }
+})
+
+gsap.from(".service-main .box",{
+    scale:0.5,
+    duration:2,
+    scrollTrigger:{
+        // markers:true,
+        start:"top 100%",
+        end:"bottom 100%",
+        trigger:".service-main .box ",
+        scroller:"body",
+        scrub:2,
+    }
+})
+
+// Portfolio Section 
+
+gsap.from(".main-portfolio .box",{
+    scale:0.5,
+    duration:2,
+    scrollTrigger:{
+        // markers:true,
+        start:"top 100%",
+        end:"bottom 100%",
+        trigger:".main-portfolio .box ",
+        scroller:"body",
+        scrub:2,
+    }
+})
+
+gsap.from(".main-contact",{
+    scale:0.5,
+    duration:2,
+    scrollTrigger:{
+        // markers:true,
+        start:"top 100%",
+        end:"bottom 100%",
+        trigger:".main-contact",
+        scroller:"body",
+        scrub:2,
+    }
+})
+
+// Lodder 
+// ================>
+
+
+function time(){
+    var a = 0;
+    setInterval(() => {
+         a  += Math.floor(Math.random()*15)
+        if(a<100){
+            document.querySelector("#loader h1").innerHTML=a+"%";   
+        }else{
+            a=100;
+            document.querySelector("#loader h1").innerHTML=a+"%";   
+        }
+    },70);
+}
+
+time()
+
+
+let tlload = gsap.timeline() 
+
+tlload.to("#loader h1",{
+   scale:2.5,
+   delay:0.3,
+})
+
+tlload.to("#loader",{
+    top:"-100vh",
+    delay:0.5,
+    duration:2, 
+})
+
+
+setInterval(()=>{
+  let a  = document.getElementById("loader");
+  a.style.transform="translateX : -100%" 
+},1000)
+
+
+let fnav = document.getElementById("fnav")
+let snav = document.getElementById("snav")
+let tnav = document.getElementById("tnav")
+let fonav = document.getElementById("fonav")
+let finav = document.getElementById("finav")
+
+
+//  For Animation Writing 
+
+
+var auto_text = document.getElementById("change")
+
+var color= ["crimson","green"]; 
+
+var typed = new Typed("#change",{
+  strings : ["Rohit", "Programmer" ,"Youtuber" , "Developer"],
+  typeSpeed:150,
+  backSpeed:150,
+  loop:true,    
+}) 
+
+
+
+// let resumeBtn = document.getElementById("resumeBtn");
+
+// resumeBtn.addEventListener("click" , async () =>{
+     
+//      const url="https://drive.google.com/file/d/1PewG4hF4KUiPHylIyHL242xFMxpYzyux/view?usp=sharing";
+
+//     const response = await fetch(url);
+
+//     const blob = await response.blob();
+//     const downloadLink = document.createElement("a")
+//     downloadLink.href= URL.createObjectURL(blob);
+//     downloadLink.download = "rohitkumarsahu-resume.pdf";
+//     downloadLink.click()
+
+
+// })
+
+
+
+
+
+
+
+
+
+
